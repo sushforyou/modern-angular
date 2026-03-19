3665e0c6e057924ac3e6b6768eda72542de56993
Sushant Kunkekar <kunkekarsushant@gmail.com>
2026-03-19 15:35:26 +0530

    feat: enhance forms and data binding components with improved layout, styling, and code snippets

--- src/app/components/forms/forms.ts
+++ src/app/components/forms/forms.ts
@@ -24,4 +24,9 @@ export class Forms {
     email: ['', [Validators.email, Validators.required, Validators.minLength(5)]],
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
+
+  // Code snippets to display in the template (right column)
+  codeGroup = `formGroupDemo = new FormGroup({\n  email: new FormControl('', [Validators.email, Validators.required, Validators.minLength(5)]),\n  password: new FormControl('', [Validators.required, Validators.minLength(6)]),\n})`;
+
+  codeBuilder = `fb = inject(FormBuilder);\nformBuilderDemo = this.fb.nonNullable.group({\n  email: ['', [Validators.email, Validators.required, Validators.minLength(5)]],\n  password: ['', [Validators.required, Validators.minLength(6)]],\n}); Also its non nullable after RESET`;
 }
